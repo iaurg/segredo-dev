fcae86280993b927cda5dc728037904d6a1538e5
Italo Aurelio <italoaurelior@gmail.com>
2021-06-25 23:11:44 -0300

    Change footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,14 +5,7 @@ const Footer = () => {
     <footer className="my-12 text-center">
       © {new Date().getFullYear()}, Built with
       {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a> and{" "}
-      <a
-        href="https://github.com/renyuanz/leonids"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Leonids theme
-      </a>
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
       .
     </footer>
   )
